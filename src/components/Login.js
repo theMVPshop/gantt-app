c2780c6432eeadf8de989aede434388298dd08a9
pjwhite08 <pamelajeanwhite@gmail.com>
2022-03-30 19:57:44 -0500

    Router working, two routes; login component begin to style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,31 @@
-import React from "react"
+import * as React from "react"
+import { Link } from "react-router-dom"
+
+import "./Login.css";
 
 
 const Login = () => {
 
   return (
+    <div className="login-window-container">
       <div className="login-window">
-          <h1>Login</h1>
-          <input type="text" className="username">Username</input>
-          <input type="text" className="Password">Password</input>
+        <h1>Login</h1>
+        <form className="login-form">
+          <div className="input-group">
+            <label for="username">Username:  </label>
+            <input type="text" className="username" name="username"></input>
+          </div>
+          <br/>
+          <div className="input-group">
+            <label for="password">Password: </label>
+            <input type="password" className="password" name="password"></input>
+          </div>
+          <br/>
+          
+          <Link to="/dashboard">Dashboard</Link>
+        </form>
       </div>
+    </div>
   )
 }
 
