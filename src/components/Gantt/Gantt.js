e0667cbc6cb851060ee9b60f7dae596770f10895
JasonCornish5 <jason.cornish5@yahoo.com>
2022-03-31 19:59:09 -0500

    added basic functionality for the custom add buttons

--- src/components/Gantt/Gantt.js
+++ src/components/Gantt/Gantt.js
@@ -5,21 +5,12 @@ import CohortForm from "../Forms/CohortForm.js";
 import "dhtmlx-gantt/codebase/dhtmlxgantt.css";
 import "./Gantt.css";
 
-const Gantt = (props) => {
-  const { tasks } = props;
+const Gantt = () => {
   const containerRef = useRef(null);
 
   const [courseFormDisplay, setCourseFormDisplay] = useState(false);
 
-  // useEffect(() => {
-
-  // }, []);
-
-  const addRow = (e) => {
-    console.log(e.target);
-  };
-
-  //Following variables are for declaring svg icons that DHTMLX requires to be read as inline html (non JSX) elements
+  //variables are for declaring our svg icons. DHTMLX Gantt requires custom icons to be stored as inline html (non JSX) elements
   var plusIconRow =
     "<svg id='plusIconRow' data-action='add' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M19,11H13V5a1,1,0,0,0-2,0v6H5a1,1,0,0,0,0,2h6v6a1,1,0,0,0,2,0V13h6a1,1,0,0,0,0-2Z'/></svg>";
 
@@ -32,19 +23,63 @@ const Gantt = (props) => {
   var editIcon =
     "<svg id='editIcon' data-action='edit' width='20' height='20' xmlns='http://www.w3.org/2000/svg' version='1.1' xmlnsXlink='http://www.w3.org/1999/xlink' xmlnsSvgjs='http://svgjs.com/svgjs'><defs id='SvgjsDefs1002'></defs><g id='SvgjsG1008'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='20' height='20'><path d='M22,7.24a1,1,0,0,0-.29-.71L17.47,2.29A1,1,0,0,0,16.76,2a1,1,0,0,0-.71.29L13.22,5.12h0L2.29,16.05a1,1,0,0,0-.29.71V21a1,1,0,0,0,1,1H7.24A1,1,0,0,0,8,21.71L18.87,10.78h0L21.71,8a1.19,1.19,0,0,0,.22-.33,1,1,0,0,0,0-.24.7.7,0,0,0,0-.14ZM6.83,20H4V17.17l9.93-9.93,2.83,2.83ZM18.17,8.66,15.34,5.83l1.42-1.41,2.82,2.82Z' fill='#080000' class='color000 svgShape'></path></svg></g></svg>";
 
-  //When DOM content is loaded, this sets our custom Gantt columns
+  //state for storing data, currently filled with dummy data
+  const [data, setData] = useState({
+    data: [
+      {
+        id: 1,
+        text: "Task #1",
+        start_date: "2019-04-15",
+        duration: 3,
+        progress: 0.6,
+      },
+      {
+        id: 2,
+        text: "Task #2",
+        start_date: "2019-04-18",
+        duration: 3,
+        progress: 0.4,
+      },
+      {
+        id: 3,
+        text: "Task #3",
+        start_date: "2019-04-18",
+        duration: 3,
+        progress: 0.4,
+      },
+    ],
+    links: [{ id: 1, source: 1, target: 2, type: "0" }],
+  });
+
+  //monitors data and re-renders gantt chart if change detected
+  useEffect(() => {
+    gantt.parse(data);
+  }, [data]);
+
+  //when DOM content is loaded, this sets our custom Gantt columns
   document.addEventListener("DOMContentLoaded", (event) => {
     gantt.config.date_format = "%d-%m-%y";
     gantt.init(containerRef.current);
-    gantt.parse(tasks);
 
+    //onclick function for plus icon in header
     document.addEventListener("click", (e) => {
       e.preventDefault();
       if (e.target.id === "plusIconHeader") {
-        console.log("test");
+        setData((prevState) => {
+          var copy = { ...prevState };
+          copy.data.push({
+            id: 4,
+            text: "Task #4",
+            start_date: "2019-04-18",
+            duration: 2,
+            progress: 0.4,
+          });
+          return copy;
+        });
       }
     });
 
+    //gantt custom columns
     gantt.config.columns = [
       {
         name: "text",
@@ -94,6 +129,7 @@ const Gantt = (props) => {
       },
     ];
 
+    //onclick listener for custom buttons in row
     gantt.attachEvent("onTaskClick", function (id, e) {
       var button = e.target.closest("[data-action]");
       if (button) {
