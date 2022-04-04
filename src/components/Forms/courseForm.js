098219a0094198714267265b8f53ffb2c9621aef
chase-powell <landon.powel4@gmail.com>
2022-04-04 18:58:06 -0500

    added changes to courseForm

--- src/components/Forms/courseForm.js
+++ src/components/Forms/courseForm.js
@@ -7,50 +7,51 @@ const CourseForm = (props) => {
       <dic className="courseContainer">
 
       <div className="labels">
-          <label id="label">Course Number</label>
-          <label id="label">Course Link</label>
-          <label id="label">Hubspot Ticket</label>
-          <label id="label">Rocket Chat</label>
-          <label id="label">Instructor</label>
-          <label id="label">Teacher Assistant</label>
-          <label id="label">Location</label>
-          <label id="label">Days</label>
-          <label id="label">Mode</label>
-          <label id="label">Start Date</label>
-          <label id="label">End Date</label>
-          <label id="label">Number of Students Starting</label>
-          <label id="label">Number of Students Ending</label>
-          <label className="switch" id="label">Active Status</label>
+          <label className="label-1">Course Number</label>
+          <label className="label-1">Course Link</label>
+          <label className="label-1">Hubspot Ticket</label>
+          <label className="label-1">Rocket Chat</label>
+          <label className="label-1">Instructor</label>
+          <label className="label-1">Teacher Assistant</label>
+          <label className="label-1">Location</label>
+          <label className="label-1">Days</label>
+          <label className="label-1">Mode</label>
+          <label className="label-1">Start Date</label>
+          <label className="label-1">End Date</label>
+
+          <label className="label-2">Number of Students Starting</label>
+          <label className="label-2">Number of Students Ending</label>
+          <label className="label-2" >Active Status</label>
         </div>
 
         <div className="inputs">
-          <input type="text" name="courseNum" id="input" />
-          <input type="text" name="courseLink" id="input" />
-          <input type="text" name="hubspotTicket" id="input" />
-          <input type="text" name="rocketChat"  id="input"/>
-          <input type="text" name="instructor" id="input"/>
-          <input type="text" name="teacherAssistant" id="input" />
-          <select id="input">
+          <input type="text" name="courseNum" className="text-input" />
+          <input type="text" name="courseLink" className="text-input" />
+          <input type="text" name="hubSpotTicket" className="text-input" />
+          <input type="text" name="rocketChat"  className="text-input"/>
+          <input type="text" name="instructor" className="text-input"/>
+          <input type="text" name="teacherAssistant" className="text-input" />
+          <select className="select-input">
             <option value="acaStEdwards">ACA - St. Edwards</option>
             <option value="lca">LCA</option>
             <option value="acaOnlineNorth">ACA - Online-North</option>
             <option value="ttcu">TTCU</option>
             <option value="ttcuMarbleFalls">TTCU - Marble Falls</option>
           </select>
-          <select id="input">
+          <select className="select-input">
             <option value="partTimeMonWed">Mon/Wed</option>
             <option value="partTimeTuesThurs">Tues/Thurs</option>
             <option value="fullTime">Full Time</option>
           </select>
-          <select id="input">
+          <select className="select-input">
             <option value="online">Online</option>
             <option value="inPerson">In-Person</option>
           </select>
-          <input type="date" name="startDate" id="input"/>
-          <input type="date" name="endDate" id="input" />
-          <input type="number" name="studentNumStart" id="input" />
-          <input type="number" name="studentNumEnd" id="input" />
-          <input type="checkbox" id="input" />
+          <input type="date" name="startDate" className="select-input"/>
+          <input type="date" name="endDate" className="select-input" />
+          <input type="number" name="studentNumStart" className="last-input" />
+          <input type="number" name="studentNumEnd" className="last-input" />
+          <input type="checkbox" className="last-input" />
           <span className="slider round"></span>
         </div>
 
