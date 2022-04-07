e457d621879bedeb2c6b9a3af96086851dafc1d6
pjwhite08 <pamelajeanwhite@gmail.com>
2022-04-06 19:57:57 -0500

    login works again

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "./components/Dashboard";
-import Login from "./components/Login";
+import Login from "./components/Login/Login.js";
 import Signup from "./components/Signup/Signup.js";
 
 import CohortForm from "./components/Forms/CohortForm";
