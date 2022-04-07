532c2cff473fbecf28f0763523645c663a83ec7e
JasonCornish5 <jason.cornish5@yahoo.com>
2022-04-07 13:44:23 -0500

    added basic form tracking to courseform component

--- src/components/Forms/courseForm.js
+++ src/components/Forms/courseForm.js
@@ -1,9 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const CourseForm = (props) => {
   // useEffect(() => {
   //   console.log(props.courseDisplay.id);
   // }, [props.courseDisplay.id]);
+
+  const [formData, setFormData] = useState({
+    courseNum: 0,
+    courseLink: 0,
+    hubSpotTicket: 0,
+    rocketChat: 0,
+    instructor: 0,
+    teacherAssistant: 0,
+    location: 0,
+    days: 0,
+    mode: 0,
+    startDate: 0,
+    endDate: 0,
+    startStudents: 0,
+    endStudents: 0,
+    activeStatus: 0,
+  });
 
   return (
     <div>
@@ -17,36 +34,111 @@ const CourseForm = (props) => {
       >
         <div className="info">
           <label className="label">Course Number</label>
-          <input type="text" name="courseNum" className="input" />
+          <input
+            type="text"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.courseNum = e.target.value;
+                return prev;
+              });
+            }}
+            name="courseNum"
+            className="input"
+          />
         </div>
 
         <div className="info">
           <label className="label">Course Link</label>
-          <input type="text" name="courseLink" className="input" />
+          <input
+            type="text"
+            name="courseLink"
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.courseLink = e.target.value;
+                return prev;
+              });
+            }}
+          />
         </div>
 
         <div className="info">
           <label className="label">Hubspot Ticket</label>
-          <input type="text" name="hubSpotTicket" className="input" />
+          <input
+            type="text"
+            name="hubSpotTicket"
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.hubSpotTicket = e.target.value;
+                return prev;
+              });
+            }}
+          />
         </div>
 
         <div className="info">
           <label className="label">Rocket Chat</label>
-          <input type="text" name="rocketChat" className="input" />
+          <input
+            type="text"
+            name="rocketChat"
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.rocketChat = e.target.value;
+                return prev;
+              });
+            }}
+          />
         </div>
 
         <div className="info">
           <label className="label">Instructor</label>
-          <input type="text" name="instructor" className="input" />
+          <input
+            type="text"
+            name="instructor"
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.instructor = e.target.value;
+                return prev;
+              });
+            }}
+          />
         </div>
         <div className="info">
           <label className="label">Teacher Assistant</label>
-          <input type="text" name="teacherAssistant" className="input" />
+          <input
+            type="text"
+            name="teacherAssistant"
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.teacherAssistant = e.target.value;
+                return prev;
+              });
+            }}
+          />
         </div>
 
         <div className="info">
           <label className="label">Location</label>
-          <select className="input">
+          <select
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.location = e.target.value;
+                return prev;
+              });
+            }}
+          >
             <option value="acaStEdwards">ACA - St. Edwards</option>
             <option value="lca">LCA</option>
             <option value="acaOnlineNorth">ACA - Online-North</option>
@@ -57,7 +149,16 @@ const CourseForm = (props) => {
 
         <div className="info">
           <label className="label">Days</label>
-          <select className="input">
+          <select
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.days = e.target.value;
+                return prev;
+              });
+            }}
+          >
             <option value="partTimeMonWed">Mon/Wed</option>
             <option value="partTimeTuesThurs">Tues/Thurs</option>
             <option value="fullTime">Full Time</option>
@@ -66,7 +167,16 @@ const CourseForm = (props) => {
 
         <div className="info">
           <label className="label">Mode</label>
-          <select className="input">
+          <select
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.mode = e.target.value;
+                return prev;
+              });
+            }}
+          >
             <option value="online">Online</option>
             <option value="inPerson">In-Person</option>
           </select>
@@ -74,12 +184,34 @@ const CourseForm = (props) => {
 
         <div className="info">
           <label className="label">Start Date</label>
-          <input type="date" name="startDate" className="input" />
+          <input
+            type="date"
+            name="startDate"
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.startDate = e.target.value;
+                return prev;
+              });
+            }}
+          />
         </div>
 
         <div className="info">
           <label className="label">End Date</label>
-          <input type="date" name="endDate" className="input" />
+          <input
+            type="date"
+            name="endDate"
+            className="input"
+            onChange={(e) => {
+              setFormData((prevState) => {
+                let prev = { ...prevState };
+                prev.endDate = e.target.value;
+                return prev;
+              });
+            }}
+          />
         </div>
 
         <div className="info">
--- src/components/Gantt/Gantt.js
+++ src/components/Gantt/Gantt.js
@@ -69,38 +69,14 @@ const Gantt = () => {
 
   //monitors data and re-renders gantt chart if change detected
   useEffect(() => {
-    gantt.clearAll();
-    gantt.parse({
-      data: [
-        { id: 1, text: "Project #2", start_date: "01-04-2013", duration: 18 },
-        {
-          id: 2,
-          text: "Task #1",
-          start_date: "02-04-2013",
-          duration: 8,
-          progress: 0.6,
-          parent: 1,
-        },
-        {
-          id: 3,
-          text: "Task #2",
-          start_date: "11-04-2013",
-          duration: 8,
-          progress: 0.6,
-          parent: 1,
-        },
-      ],
-      links: [
-        { id: 1, source: 1, target: 2, type: 1 },
-        { id: 2, source: 2, target: 3, type: 0 },
-      ],
-    });
+    gantt.parse(data);
   }, [data]);
 
   //when DOM content is loaded, this sets our custom Gantt columns
   document.addEventListener("DOMContentLoaded", (event) => {
     gantt.config.date_format = "%Y-%m-%d %H:%i";
     gantt.init(containerRef.current);
+    gantt.parse(data);
 
     //onclick function for plus icon in header
     document.addEventListener("click", (e) => {
@@ -181,7 +157,7 @@ const Gantt = () => {
             break;
           case "add":
             console.log(id);
-            setCohortFormDisplay({ display: true, id: id });
+            setCourseFormDisplay({ display: true, id: id });
             break;
           case "delete":
             console.log(id);
