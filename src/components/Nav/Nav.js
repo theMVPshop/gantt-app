ebec14501a8c70e1f6f1771eb3b3a9054f2d2e47
mayrant <mayrant2004@yahoo.com>
2022-04-28 19:09:03 -0500

    deleted comments

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,37 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import { checkAuth } from "../../Router";
-import CohortForm from "../Forms/CohortForm.js";
 import "./Nav.css";
 
 const Nav = () => {
-  // confirming that this is 
-  // const [cohortFormDisplay, setCohortFormDisplay] = useState({
-  //   display: true,
-  // });
-
   const currentPath = window.location.pathname;
-  console.log(currentPath)
 
   return (
     <nav className="nav-bar">
       { checkAuth() && <p className="name-tag">Welcome, {localStorage.getItem("user_name")}</p> }
-      {/* { checkAuth() ? 
-        <button className="nav-link">
-          <Link to="/" className="link"
-            onClick={() => {
-            document.cookie = "loggedIn="
-            localStorage.clear()
-            window.location.replace("/")}}
-          >
-            Logout
-          </Link>
-        </button> : 
-        <button className="nav-link">
-          <Link className="link" to="/signup">Sign Up</Link>
-        </button> 
-      } */}
       { checkAuth() ? 
         <button className="nav-link">
           <Link to="/" className="link"
