46858dd998bea9874f5262621e2002d9a1bd2b48
JasonCornish5 <jason.cornish5@yahoo.com>
2022-05-10 18:52:50 -0500

    cohort id name duplication verification added

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -13,47 +13,63 @@ const Nav = () => {
     document.cookie = "loggedIn=";
     localStorage.clear();
     window.location.replace("/");
-  }
+  };
 
-  useEffect(() => {
-    const timer = new IdleTimer({
-      timeout: 5, //expire after 5 seconds
-      onTimeout: () => {
-        setIsTimeout(true);
-        logoutUser();
-      },
-      onExpired: () => {
-        //do something if expired on load
-        setIsTimeout(true);
-        logoutUser();
-      }
-    });
+  // useEffect(() => {
+  //   const timer = new IdleTimer({
+  //     timeout: 5, //expire after 5 seconds
+  //     onTimeout: () => {
+  //       setIsTimeout(true);
+  //       logoutUser();
+  //     },
+  //     onExpired: () => {
+  //       //do something if expired on load
+  //       setIsTimeout(true);
+  //       logoutUser();
+  //     }
+  //   });
 
-    return () => {
-      timer.cleanUp();
-    };
-  }, []);
+  //   return () => {
+  //     timer.cleanUp();
+  //   };
+  // }, []);
 
   return (
     <nav className="nav-bar">
-      { checkAuth() && <p className="name-tag">Welcome, {localStorage.getItem("user_name")}</p> }
-      { checkAuth() ? 
+      {checkAuth() && (
+        <p className="name-tag">Welcome, {localStorage.getItem("user_name")}</p>
+      )}
+      {checkAuth() ? (
         <button className="nav-link">
-          <Link to="/" className="link"
-            onClick={logoutUser}
-          >
+          <Link to="/" className="link" onClick={logoutUser}>
             Logout
           </Link>
         </button>
-        :
+      ) : (
         <button className="nav-link">
-          { 
-            currentPath === "/" ? 
-            <Link className="link" to="/signup" onClick={() => {window.location.replace("/signup")}}>Sign Up</Link> 
-            : <Link className="link" to="/" onClick={() => {window.location.replace("/")}}>Sign In</Link> 
-          }
-        </button>  
-      }
+          {currentPath === "/" ? (
+            <Link
+              className="link"
+              to="/signup"
+              onClick={() => {
+                window.location.replace("/signup");
+              }}
+            >
+              Sign Up
+            </Link>
+          ) : (
+            <Link
+              className="link"
+              to="/"
+              onClick={() => {
+                window.location.replace("/");
+              }}
+            >
+              Sign In
+            </Link>
+          )}
+        </button>
+      )}
     </nav>
   );
 };
