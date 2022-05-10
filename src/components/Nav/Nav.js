223ab3e46549456f62f129167ce5f233fb15bee4
mayrant <mayrant2004@yahoo.com>
2022-05-09 19:26:59 -0500

    idle time logout function in nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { checkAuth } from "../../Router";
+import IdleTimer from "../IdleTimer";
 import "./Nav.css";
 
 const Nav = () => {
   const currentPath = window.location.pathname;
+
+  const [isTimeout, setIsTimeout] = useState(false);
+  useEffect(() => {
+    const timer = new IdleTimer({
+      timeout: 5, //expire after 10 seconds
+      onTimeout: () => {
+        setIsTimeout(true);
+        document.cookie = "loggedIn=";
+        localStorage.clear();
+        window.location.replace("/");
+      },
+      onExpired: () => {
+        //do something if expired on load
+        setIsTimeout(true);
+        document.cookie = "loggedIn=";
+        localStorage.clear();
+      }
+    });
+
+    return () => {
+      timer.cleanUp();
+    };
+  }, []);
 
   return (
     <nav className="nav-bar">
