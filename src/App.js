15d9e5f21741c5e3e3e58da9ca3dbe57adcc06cf
mayrant <mayrant2004@yahoo.com>
2022-05-10 19:05:19 -0500

    removed unused lines

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import Router, { logoutUser } from "./Router.js";
