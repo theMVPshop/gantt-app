2cb7d9e7d9cb9fb836e3a7c09bdd6892ee27115d
pjwhite08 <pamelajeanwhite@gmail.com>
2022-05-12 19:53:51 -0500

    required fields on add cohort are working

--- src/components/Forms/CohortForm.js
+++ src/components/Forms/CohortForm.js
@@ -15,17 +15,22 @@ const CohortForm = (props) => {
   } = useForm();
 
 const requiredFields ={
-  cohortName: { required: "Cohort Name is required (this can be changed later)"},
-  startDate: { required: "Start Date is required (this can be changed later)"},
-  graduationDate: { required: "Graduation Date is required (this can be changed later)"}
+  cohortName:  "Cohort Name is required (this can be changed later)",
+  startDate: "Start Date is required (this can be changed later)",
+  graduationDate: "Graduation Date is required (this can be changed later)",
 }
 
   const [formData, setFormData] = useState({
     id: "cohort_0",
     title: "",
-    start_date: "yyyy-mm-dd",
-    end_date: "yyyy-mm-dd",
+    start_date: "",
+    end_date: "",
+    title_error: false,
+    start_date_error: false,
+    graduation_date_error: false,
   });
+
+ 
 
   const resetForm = () => {
     setFormData({
@@ -41,38 +46,55 @@ const requiredFields ={
 
   const pushFormData = () => {
     var cohortCounter = 1;
-    // if (formData.title == "default value"){
-      
-    // }
-    var idArray = [];
-    for (let i = 0; i < props.data.data.length; i++) {
-      if (props.data.data[0].id) {
-        let cohortIDArray = props.data.data[i].id.split("_");
-        if (cohortIDArray[0] === "cohort") {
-          cohortCounter++;
-          idArray.push(cohortIDArray[1]);
-        }
-      } else idArray.push("1");
+  
+    if (formData.title == ""){
+      setFormData({title_error: true} )
+      return
+    }
+    
+    if (formData.start_date == ""){
+      console.log("There's no start date")
+      setFormData({ start_date_error: true })
+      return
     }
 
-    if (idArray.length > 0) {
-      //protects against 'course_infinity" id being created
-      const newID = Math.max(...idArray) + 1;
-      formData.id = `cohort_${newID}`;
-    } else {
-      formData.id = "cohort_1";
+    if (formData.end_date == ""){
+      console.log("There's no graduation date")
+      setFormData({ graduation_date_error: true })
+      return
     }
-
-    axios
-      .post(url, formData)
-      .then((res) => {
-        if (res.status === 200) {
-          props.customAddTask(formData);
-          resetForm();
-        }
-      })
-      .catch((err) => console.log("there was an error", err));
-    gantt.open(props.modalState.addCohortForm.id); //forces open the parent task
+    
+    
+      var idArray = [];
+      for (let i = 0; i < props.data.data.length; i++) {
+        if (props.data.data[0].id) {
+          let cohortIDArray = props.data.data[i].id.split("_");
+          if (cohortIDArray[0] === "cohort") {
+            cohortCounter++;
+            idArray.push(cohortIDArray[1]);
+          }
+        } else idArray.push("1");
+      }
+      if (idArray.length > 0) {
+        //protects against 'course_infinity" id being created
+        const newID = Math.max(...idArray) + 1;
+        formData.id = `cohort_${newID}`;
+      } else {
+        formData.id = "cohort_1";
+      }
+  
+      axios
+        .post(url, formData)
+        .then((res) => {
+          if (res.status === 200) {
+            props.customAddTask(formData);
+            resetForm();
+          }
+        })
+        .catch((err) => console.log("there was an error", err));
+      gantt.open(props.modalState.addCohortForm.id); //forces open the parent task
+    
+    
   };
 
   return (
@@ -117,13 +139,11 @@ const requiredFields ={
             }}
             name="title"
             className="input"
-            // {...register("title", requiredFields.cohortName)}
           />
         </div>
         <small className="text-danger">
-
           {/* if titleErrors state is true display here */}
-          {/* {errors?.cohortName && errors.cohortName.message} */}
+          {formData.title_error && requiredFields.cohortName}
         </small>
 
         <div className="info">
@@ -145,7 +165,7 @@ const requiredFields ={
           />
         </div>
         <small className="text-danger">
-          {errors?.startDate && errors.startDate.message}
+        {formData.start_date_error && requiredFields.startDate}
         </small>
 
         <div className="info">
@@ -166,7 +186,7 @@ const requiredFields ={
           />
         </div>
         <small className="text-danger">
-          {errors?.graduationDate && errors.graduationDate.message}
+        {formData.graduation_date_error && requiredFields.graduationDate}
         </small>
         <h6 className="required">
           <em>*required</em>
