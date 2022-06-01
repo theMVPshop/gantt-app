afbab90c924a7ec756950ac0660a467ec2baa741
mayrant <mayrant2004@yahoo.com>
2022-05-31 19:32:05 -0500

    deleted unused comments

--- src/components/Displays/CourseDisplay.js
+++ src/components/Displays/CourseDisplay.js
@@ -91,8 +91,6 @@ const CourseDisplay = (props) => {
             <div className="display-info">
               <label className="display-label">Days:</label>
               <div className="display-input">
-                {/* call a function that returns three possible versions of this */}
-                {/* {displayDays(props)} */}
                 {props.modalState.currentTask.day_of_week}
               </div>
             </div>
