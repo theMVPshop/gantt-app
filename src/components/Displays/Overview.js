6d44cfa20bd379c04ab0f70950496c0fe89483b7
pjwhite08 <pamelajeanwhite@gmail.com>
2022-06-09 19:37:32 -0500

    overviewDisplay styled, showing correct days, years

--- src/components/Displays/Overview.js
+++ src/components/Displays/Overview.js
@@ -9,6 +9,7 @@ const OverviewDisplay = (props) => {
   //based on the currentTask, which is a cohort
   const [overviewDays, setOverviewDays] = useState("")
   const [overview501, setOverview501] = useState("")
+  const [overview501Days, setOverview501Days] = useState("")
   const [overview501StartDate, setOverview501StartDate] = useState("")
   const [overview501EndDate, setOverview501EndDate] = useState("")
   let coursesArray = []
@@ -18,21 +19,23 @@ const OverviewDisplay = (props) => {
     for (let i = 0; i<arr.length; i++){
       //find all the courses where data parent is equal to currentTask id
       if(arr[i].parent == props.modalState.currentTask.id && arr[i].id){
-        //if there's a day_of_week, set the day_of_week
-        // this does NOT necessarily find the most recent day_of_week, if it differs across courses
-        if(arr[i].day_of_week){
-          setOverviewDays(arr[i].day_of_week)
-        }
+        
         
         //if there's a 501 course, set local state 
         if(arr[i].title.includes("501")) {
           setOverview501("501")
-          setOverview501StartDate(arr[i].start_date.toString().slice(0, 15))
-          setOverview501EndDate(arr[i].end_date.toString().slice(0, 15))
+          setOverview501Days(arr[i].day_of_week)
+          setOverview501StartDate(arr[i].start_date.toString().slice(3, 11))
+          setOverview501EndDate(arr[i].end_date.toString().slice(3, 15))
         }
 
         //get all the courses that are not 501
         if(arr[i].title.indexOf('501') === -1) {
+          //if there's a day_of_week, set the day_of_week
+        // this does NOT necessarily find the most recent day_of_week, if it differs across courses
+          if(arr[i].day_of_week){
+            setOverviewDays(arr[i].day_of_week)
+          }
           console.log("arr[i].title is a course, not 501: ", arr[i].title)
           let courseObj = {
             title: arr[i].title,
@@ -57,25 +60,28 @@ const OverviewDisplay = (props) => {
             : ""}
       {overviewCourses.map((course, index)=>
         <>
-        <span>{course.title}</span>
-        <span>{course.start_date.toString().slice(0, 15)}</span> - 
-        <span>{course.end_date.toString().slice(0, 15)}</span>
+        <span className="overviewLeftItem">{course.title}:</span>
+        {course.start_date.toString().slice(12, 15) == course.end_date.toString().slice(12, 15) ? <span> {course.start_date.toString().slice(3, 11)}</span> : <span> {course.start_date.toString().slice(3, 15)}</span> } - 
+        <span>{course.end_date.toString().slice(3, 15)}</span>
 
         <br></br>
         </>
       )}
   
         <br></br>
-        Graduation Date: 
+        <span className="overviewLeftItem">Graduation Date: </span>
           {props.modalState.currentTask.end_date
-            ? <>{props.modalState.currentTask.end_date.toString().slice(0, 15)} <br></br>
-            <br></br></>
+            ? <span >{props.modalState.currentTask.end_date.toString().slice(0, 15)} <br></br>
+            <br></br></span>
             : ""}
         {overview501
-            ? <>{overview501}<span>{overview501StartDate}</span>- <span>{overview501EndDate}</span><br></br>
-            <br></br></>
+            ? <>
+              <span className="overviewLeftItem">{overview501}:</span>
+              <span className="overviewLeftItem">{overview501Days}</span>
+              <span>{overview501StartDate}</span>- <span>{overview501EndDate}</span>
+        
+            </>
             : ""}
-        <br></br>
       </p>
     </div>
   );
